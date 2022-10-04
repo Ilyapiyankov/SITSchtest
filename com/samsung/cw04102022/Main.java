12e1fd49056c7388e7c7b3ea3ba56265f7d61087
Ilyapiyankov <ilya.piyankov@gmail.com>
2022-10-04 20:18:56 +0300

    test

---
+++ com/samsung/cw04102022/Main.java
@@ -0,0 +1,12 @@
+package com.samsung.cw04102022;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        boolean b = 90-77>90 || "t" =="c";
+        System.out.println(b  ? 90 : "jkhj");
+
+    }
+
+}
