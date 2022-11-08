7c55f975266985baef140d96e41f343ebac3148a
Ilyapiyankov <ilya.piyankov@gmail.com>
2022-11-08 19:05:53 +0300

    work

---
+++ com/samsung/MOD2/cw03112022/HW/Main.java
@@ -0,0 +1,13 @@
+package SITSchtest.com.samsung.MOD2.cw03112022.HW;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+class Duplicator {
+    public static String str(String s){
+        return s+s;
+    }
+}
