3c1b6380259a8c70ece5f79bc3bfb7be81b8d290
Ilyapiyankov <ilya.piyankov@gmail.com>
2022-11-30 19:33:14 +0300

    work29112022 end2.2

--- com/samsung/MOD2/cw29112022/Main.java
+++ com/samsung/MOD2/cw29112022/Main.java
@@ -1,11 +1,6 @@
 package SITSchtest.com.samsung.MOD2.cw29112022;
 
-import SITSchtest.com.samsung.MOD2.cw29112022.Model.Student;
-import SITSchtest.com.samsung.MOD2.cw29112022.Model.Teacher;
-import SITSchtest.com.samsung.MOD2.cw29112022.Model.University;
 import SITSchtest.com.samsung.MOD2.cw29112022.Service.DemoService;
-
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
